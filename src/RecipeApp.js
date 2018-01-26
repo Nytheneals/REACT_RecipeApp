0bc14162567449d2387eb81fe4e748d7dae5ac36
Nytheneals <nythes@gmail.com>
2018-01-26 11:27:58 -0500

    Added CSS files

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import './RecipeApp.css';
-import Recipe from './Recipe';
+import React, { Component } from "react";
+import "./RecipeApp.css";
+import Recipe from "./Recipe";
+import Navbar from "./Navbar";
 
 class RecipeApp extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="App-title">RECIPE APP</h1>
+        <Navbar />
         <Recipe
           title="Pasta"
-          ingredients={['Water', 'Garlic']}
+          ingredients={["Water", "Garlic"]}
           instructions="Mix ingredients"
-          img="QKFxPAw.jpg"
+          img="pasta.jpg"
         />
       </div>
     );
