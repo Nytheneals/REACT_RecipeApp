84130c69dc33b8dd5dce307042ae2bc6ae418735
Nytheneals <nythes@gmail.com>
2018-01-28 12:12:36 -0500

    Edited Recipe List

--- src/Recipelist.js
+++ src/Recipelist.js
@@ -1,48 +1,46 @@
-import React, { Component } from "react";
-import Recipe from "./Recipe";
-import PropTypes from "prop-types";
-import "./RecipeList.css";
+import React, { Component } from 'react';
+import Recipe from './Recipe';
+import PropTypes from 'prop-types';
+import './RecipeList.css';
 
 class RecipeList extends Component {
   static defaultProps = {
     recipes: [
       {
-        title: "PASTA",
+        title: 'PASTA',
         instructions:
-          "Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce",
-        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-        img: "pasta.jpg"
+          'Open jar of Spaghetti sauce.  Bring to simmer.  Boil water.  Cook pasta until done.  Combine pasta and sauce',
+        ingredients: ['pasta', '8 cups water', '1 box spaghetti'],
+        img: 'pasta.jpg',
       },
       {
-        title: "STEAK",
-        instructions: "Combine ice cream and milk.  Blend until creamy",
-        ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-        img: "steak.jpeg"
+        title: 'STEAK',
+        instructions: 'Combine ice cream and milk.  Blend until creamy',
+        ingredients: ['2 Scoops Ice cream', '8 ounces milk'],
+        img: 'steak.jpeg',
       },
       {
-        title: "SUSHI",
+        title: 'SUSHI',
         instructions:
-          "Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.",
+          'Toast bread.  Slice avocado and spread on bread.  Add salt, oil, and pepper to taste.',
         ingredients: [
-          "2 slices of bread",
-          "1 avocado",
-          "1 tablespoon olive oil",
-          "1 pinch of salt",
-          "pepper"
+          '2 slices of bread',
+          '1 avocado',
+          '1 tablespoon olive oil',
+          '1 pinch of salt',
+          'pepper',
         ],
-        img: "sushi.jpeg"
-      }
-    ]
+        img: 'sushi.jpeg',
+      },
+    ],
   };
 
   static propTypes = {
-    recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+    recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
   };
 
   render() {
-    const recipes = this.props.recipes.map((r, index) => (
-      <Recipe key={index} {...r} />
-    ));
+    const recipes = this.props.recipes.map((r, index) => <Recipe key={index} {...r} />);
 
     return <div className="recipe-list">{recipes}</div>;
   }
