18d2139ceb87422eb26cec963bdf3d7af5031e4c
Nytheneals <nythes@gmail.com>
2018-03-23 16:34:55 -0400

    DefaultProps to State

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -4,72 +4,75 @@ import PropTypes from "prop-types";
 // import "../css/RecipeList.css";
 
 class RecipeList extends Component {
-  static defaultProps = {
-    recipes: [
-      {
-        title: "PASTA",
-        instructions: [
-          "Open jar of Spaghetti sauce.",
-          "Bring to simmer.",
-          "Boil water.  Cook pasta until done.",
-          "Combine pasta and sauce"
-        ],
-        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-        img: "pasta.jpg"
-      },
-      {
-        title: "PASTA",
-        instructions: [
-          "Open jar of Spaghetti sauce.",
-          "Bring to simmer.",
-          "Boil water.  Cook pasta until done.",
-          "Combine pasta and sauce"
-        ],
-        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-        img: "pasta.jpg"
-      },
-      {
-        title: "PASTA",
-        instructions: [
-          "Open jar of Spaghetti sauce.",
-          "Bring to simmer.",
-          "Boil water.  Cook pasta until done.",
-          "Combine pasta and sauce"
-        ],
-        ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-        img: "pasta.jpg"
-      },
-      {
-        title: "STEAK",
-        instructions: ["Combine ice cream and milk.", "Blend until creamy"],
-        ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
-        img: "steak.jpeg"
-      },
-      {
-        title: "SUSHI",
-        instructions: [
-          "Toast bread.",
-          "Slice avocado and spread on bread.",
-          "Add salt, oil, and pepper to taste."
-        ],
-        ingredients: [
-          "2 slices of bread",
-          "1 avocado",
-          "1 tablespoon olive oil",
-          "1 pinch of salt",
-          "pepper"
-        ],
-        img: "sushi.jpeg"
-      }
-    ]
-  };
+  constructor() {
+    super();
 
+    this.state = {
+      recipes: [
+        {
+          title: "PASTA",
+          instructions: [
+            "Open jar of Spaghetti sauce.",
+            "Bring to simmer.",
+            "Boil water.  Cook pasta until done.",
+            "Combine pasta and sauce"
+          ],
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "pasta.jpg"
+        },
+        {
+          title: "PASTA",
+          instructions: [
+            "Open jar of Spaghetti sauce.",
+            "Bring to simmer.",
+            "Boil water.  Cook pasta until done.",
+            "Combine pasta and sauce"
+          ],
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "pasta.jpg"
+        },
+        {
+          title: "PASTA",
+          instructions: [
+            "Open jar of Spaghetti sauce.",
+            "Bring to simmer.",
+            "Boil water.  Cook pasta until done.",
+            "Combine pasta and sauce"
+          ],
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "pasta.jpg"
+        },
+        {
+          title: "STEAK",
+          instructions: ["Combine ice cream and milk.", "Blend until creamy"],
+          ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
+          img: "steak.jpeg"
+        },
+        {
+          title: "SUSHI",
+          instructions: [
+            "Toast bread.",
+            "Slice avocado and spread on bread.",
+            "Add salt, oil, and pepper to taste."
+          ],
+          ingredients: [
+            "2 slices of bread",
+            "1 avocado",
+            "1 tablespoon olive oil",
+            "1 pinch of salt",
+            "pepper"
+          ],
+          img: "sushi.jpeg"
+        }
+      ]
+    };
+  }
   static propTypes = {
     recipes: PropTypes.arrayOf(PropTypes.object).isRequired
   };
 
   render() {
-    const recipes = this.props.recipes.map((r, index) => (
+    const recipes = this.state.recipes.map((r, index) => (
       <Recipe key={index} {...r} />
     ));
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import RecipeApp from "./components/RecipeApp";
-import Random from "./components/Random";
+import RecipeApp from "./components/RecipeApp";
+// import Random from "./components/Random";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<Random />, document.getElementById("root"));
+ReactDOM.render(<RecipeApp />, document.getElementById("root"));
 registerServiceWorker();
