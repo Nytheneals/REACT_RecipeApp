9baab50ac11e3b33e974eca52a373b1fe9b6f6e3
Nytheneals <nythes@gmail.com>
2018-03-23 23:32:35 -0400

    Created Input form

---
+++ src/components/RecipeInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class RecipeList extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return;
+  }
+}
+
+export default RecipeList;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -21,29 +21,29 @@ class RecipeList extends Component {
           img: "pasta.jpg"
         },
         {
-          title: "PASTA",
-          instructions: [
-            "Open jar of Spaghetti sauce.",
-            "Bring to simmer.",
-            "Boil water.  Cook pasta until done.",
-            "Combine pasta and sauce"
-          ],
-          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-          img: "pasta.jpg"
-        },
-        {
-          title: "PASTA",
-          instructions: [
-            "Open jar of Spaghetti sauce.",
-            "Bring to simmer.",
-            "Boil water.  Cook pasta until done.",
-            "Combine pasta and sauce"
-          ],
-          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
-          img: "pasta.jpg"
-        },
-        {
           title: "STEAK",
+          instructions: [
+            "Open jar of Spaghetti sauce.",
+            "Bring to simmer.",
+            "Boil water.  Cook pasta until done.",
+            "Combine pasta and sauce"
+          ],
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "pasta.jpg"
+        },
+        {
+          title: "PORK BELLY",
+          instructions: [
+            "Open jar of Spaghetti sauce.",
+            "Bring to simmer.",
+            "Boil water.  Cook pasta until done.",
+            "Combine pasta and sauce"
+          ],
+          ingredients: ["pasta", "8 cups water", "1 box spaghetti"],
+          img: "pasta.jpg"
+        },
+        {
+          title: "TOMHAWK",
           instructions: ["Combine ice cream and milk.", "Blend until creamy"],
           ingredients: ["2 Scoops Ice cream", "8 ounces milk"],
           img: "steak.jpeg"
@@ -60,7 +60,7 @@ class RecipeList extends Component {
             "1 avocado",
             "1 tablespoon olive oil",
             "1 pinch of salt",
-            "pepper"
+            "Pepper"
           ],
           img: "sushi.jpeg"
         }
@@ -68,12 +68,12 @@ class RecipeList extends Component {
     };
   }
   static propTypes = {
-    recipes: PropTypes.arrayOf(PropTypes.object).isRequired
+    recipes: PropTypes.arrayOf(PropTypes.object)
   };
 
   render() {
     const recipes = this.state.recipes.map((r, index) => (
-      <Recipe key={index} {...r} />
+      <Recipe key={r.title} {...r} />
     ));
 
     return <div className="recipe_card">{recipes}</div>;
